39444c46462330bb95609bc3b821d813acac63f3
William-Foley <folewc23@wfu.edu>
2024-09-03 10:33:31 -0400

    Try and Catch methods

--- src/Exception.java
+++ src/Exception.java
@@ -3,8 +3,15 @@ import java.util.Scanner;
 public class Exception {
     public static void main(String[] args) {
         int[] arr = new int[]{0,1,2,3,4,5};
-        System.out.println("Enter the index of the array to print its content");
         Scanner index = new Scanner(System.in);
-        System.out.println(arr[index.nextInt()]);
+        System.out.println("Enter the index of the array to print its content");
+        int x = index.nextInt();
+        try{
+            System.out.println(arr[x]);
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("The program has an exception: " + e);
+        }
+        System.out.println(arr[x]);
     }
 }
